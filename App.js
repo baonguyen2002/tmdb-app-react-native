56ad32c778922f5f869d6844c296a8b7b1fd6438
JustAlex <greenhornetnova3@gmail.com>
2024-01-03 09:37:29 +0700

    adding packages

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import MovieStack from "./Movies";
